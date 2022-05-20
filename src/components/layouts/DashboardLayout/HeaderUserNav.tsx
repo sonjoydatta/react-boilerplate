5c8b7f7d72fc7fa531a1d824c081992e5bf7033d
Sonjoy Datta <sonjoy.dat@gmail.com>
2022-05-20 18:14:39 +0600

    add extensions

--- src/components/layouts/DashboardLayout/HeaderUserNav.tsx
+++ src/components/layouts/DashboardLayout/HeaderUserNav.tsx
@@ -1,24 +1,24 @@
-import { routeNavigate } from '@/routes';
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar, Dropdown, Menu } from 'antd';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+
+const menuItems = [
+	{
+		key: 'profile',
+		label: 'Your profile',
+	},
+	{
+		key: 'account-settings',
+		label: 'Account settings',
+	},
+	{
+		key: 'sign-out',
+		label: 'Sign out',
+	},
+];
 
 export const HeaderUserNav: FC = () => (
-	<Dropdown
-		overlay={
-			<Menu>
-				<Menu.Item key={0}>
-					<Link to={routeNavigate('dashboard/profile')}>Your profile</Link>
-				</Menu.Item>
-				<Menu.Item key={1}>Account settings</Menu.Item>
-				<Menu.Divider />
-				<Menu.Item key={2}>Sign out</Menu.Item>
-			</Menu>
-		}
-		trigger={['click']}
-		placement='bottomRight'
-	>
+	<Dropdown overlay={<Menu items={menuItems} />} trigger={['click']} placement='bottomRight'>
 		<a className='ant-dropdown-link' onClick={(e) => e.preventDefault()}>
 			<Avatar size='large' icon={<UserOutlined />} />
 		</a>
